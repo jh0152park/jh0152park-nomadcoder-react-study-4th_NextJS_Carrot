0d169833e7b0f87f14767bf6341afd9ea39d9b8f
jh0152park <jh0152park@naver.com>
2024-04-19 01:52:06 +0900

    added middleware matcher

--- pepe-market/middleware.ts
+++ pepe-market/middleware.ts
@@ -2,9 +2,11 @@ import { NextRequest } from "next/server";
 import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-    switch (request.nextUrl.pathname) {
-        case "/profile":
-            const session = await getSession();
-            return Response.redirect(new URL("/", request.url));
-    }
+    const session = await getSession();
+    console.log(request.nextUrl.pathname);
+    // return Response.redirect(new URL("/", request.url));
 }
+
+export const config = {
+    matcher: ["/profile"],
+};
