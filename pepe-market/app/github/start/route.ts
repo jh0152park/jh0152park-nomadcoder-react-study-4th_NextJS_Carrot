dbab87d2168cdd8236f8d194f3329e6528e69b71
jh0152park <jh0152park@naver.com>
2024-04-19 14:06:23 +0900

    github authentication part1

---
+++ pepe-market/app/github/start/route.ts
@@ -0,0 +1,14 @@
+// The name of `route.ts` is kinda reserved name,
+// So, we can handle specific HTTP methods(like a GET or POST) of URL with route.ts file
+
+export function GET() {
+    const RequestURL = "https://github.com/login/oauth/authorize";
+    const params = {
+        client_id: process.env.GITHUB_CLIENT_ID!,
+        scope: "read:user,user:email",
+        allow_signup: "true",
+    };
+    const formattedParams = new URLSearchParams(params).toString();
+    const URL = `${RequestURL}?${formattedParams}`;
+    return Response.redirect(URL);
+}
