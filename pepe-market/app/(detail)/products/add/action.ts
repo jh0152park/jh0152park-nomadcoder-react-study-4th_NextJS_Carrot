76c6638b16975754d78806ba82236f367b254211
jh0152park <jh0152park@naver.com>
2024-04-22 17:17:02 +0900

    update product validtaion

--- pepe-market/app/(detail)/products/add/action.ts
+++ pepe-market/app/(detail)/products/add/action.ts
@@ -1,6 +1,26 @@
 "use server";
 
-export async function uploadProduct(formData: FormData) {
+import PrismaDB from "@/lib/db";
+import getSession from "@/lib/session/getSession";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const productSchema = z.object({
+    photo: z.string({
+        required_error: "Photo is required",
+    }),
+    title: z.string({
+        required_error: "Title is required",
+    }),
+    description: z.string({
+        required_error: "Description is required",
+    }),
+    price: z.coerce.number({
+        required_error: "Price is required",
+    }),
+});
+
+export async function uploadProduct(prevState: any, formData: FormData) {
     const data = {
         photo: formData.get("photo"),
         title: formData.get("title"),
@@ -8,5 +28,35 @@ export async function uploadProduct(formData: FormData) {
         description: formData.get("description"),
     };
 
-    console.log(data);
+    if (data.photo instanceof File) {
+        const photoData = await data.photo.arrayBuffer();
+    }
+
+    const result = productSchema.safeParse(data);
+
+    if (!result.success) {
+        return result.error.flatten();
+    } else {
+        const session = await getSession();
+        if (session.id) {
+            const product = await PrismaDB.product.create({
+                data: {
+                    title: result.data.title,
+                    photo: result.data.photo,
+                    price: result.data.price,
+                    description: result.data.description,
+                    user: {
+                        connect: {
+                            id: session.id,
+                        },
+                    },
+                },
+                select: {
+                    id: true,
+                },
+            });
+
+            redirect(`/products/${product.id}`);
+        }
+    }
 }
--- pepe-market/app/(detail)/products/add/page.tsx
+++ pepe-market/app/(detail)/products/add/page.tsx
@@ -5,9 +5,11 @@ import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import { uploadProduct } from "./action";
+import { useFormState } from "react-dom";
 
 export default function AddProduct() {
     const [preview, setPreview] = useState("");
+    const [state, trigger] = useFormState(uploadProduct, null);
 
     function onImageChange(event: React.ChangeEvent<HTMLInputElement>) {
         if (!event.target.files) return;
@@ -25,7 +27,7 @@ export default function AddProduct() {
 
     return (
         <div>
-            <form action={uploadProduct} className="flex flex-col gap-5 p-5">
+            <form action={trigger} className="flex flex-col gap-5 p-5">
                 <label
                     htmlFor="photo"
                     className="flex flex-col items-center justify-center bg-center bg-cover border-2 border-dashed rounded-md cursor-pointer aspect-square text-neutral-300 border-neutral-300"
@@ -55,18 +57,21 @@ export default function AddProduct() {
                     required
                     placeholder="Name of product"
                     type="text"
+                    errors={state?.fieldErrors.title}
                 />
                 <Input
                     name="price"
                     required
                     placeholder="Price of product"
                     type="number"
+                    errors={state?.fieldErrors.price}
                 />
                 <Input
                     name="description"
                     required
                     placeholder="Description of product"
                     type="text"
+                    errors={state?.fieldErrors.description}
                 />
                 <Button name="Done" />
             </form>
