31b1ee4e05a5293a59b7df5177a4ccb9ead7c0f4
jh0152park <jh0152park@naver.com>
2024-04-22 16:39:42 +0900

    upload product server action

--- pepe-market/app/(detail)/products/add/page.tsx
+++ pepe-market/app/(detail)/products/add/page.tsx
@@ -4,28 +4,49 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import { uploadProduct } from "./action";
 
 export default function AddProduct() {
     const [preview, setPreview] = useState("");
 
-    function onImageChange() {}
+    function onImageChange(event: React.ChangeEvent<HTMLInputElement>) {
+        if (!event.target.files) return;
+
+        const file = event.target.files[0];
+
+        if (file.size > 4 * 1024 * 1024) {
+            alert("Please the size of image should be less than 4MB");
+            return;
+        }
+
+        const fileURL = URL.createObjectURL(file);
+        setPreview(fileURL);
+    }
 
     return (
         <div>
-            <form className="flex flex-col gap-5 p-5">
+            <form action={uploadProduct} className="flex flex-col gap-5 p-5">
                 <label
                     htmlFor="photo"
-                    className="flex flex-col items-center justify-center border-2 border-dashed rounded-md cursor-pointer aspect-square text-neutral-300 border-neutral-300"
+                    className="flex flex-col items-center justify-center bg-center bg-cover border-2 border-dashed rounded-md cursor-pointer aspect-square text-neutral-300 border-neutral-300"
+                    style={{
+                        backgroundImage: `url(${preview})`,
+                    }}
                 >
-                    <PhotoIcon className="w-20 " />
-                    <div className="text-sm text-neutral-400">
-                        Add product's photo
-                    </div>
+                    {!preview && (
+                        <>
+                            <PhotoIcon className="w-20 " />
+                            <div className="text-sm text-neutral-400">
+                                Add product's photo
+                            </div>
+                        </>
+                    )}
                 </label>
                 <input
                     type="file"
                     id="photo"
                     name="photo"
+                    accept="image/*"
                     className="hidden"
                     onChange={onImageChange}
                 />
