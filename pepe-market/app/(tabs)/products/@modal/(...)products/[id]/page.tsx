c3db125010d5b6f67953a2a443ab3e3a20533e52
jh0152park <jh0152park@naver.com>
2024-04-24 01:18:16 +0900

    modal code challenge

--- pepe-market/app/(tabs)/products/@modal/(...)products/[id]/page.tsx
+++ pepe-market/app/(tabs)/products/@modal/(...)products/[id]/page.tsx
@@ -1,3 +1,41 @@
-export default function Modal() {
-    return <h1>Im Modal!</h1>;
+import { notFound } from "next/navigation";
+import CloseButton from "../../close-button";
+import PrismaDB from "@/lib/db";
+import Image from "next/image";
+
+export default async function Modal({ params }: { params: { id: string } }) {
+    const id = +params.id;
+    if (isNaN(id)) {
+        return notFound();
+    }
+
+    const product = await PrismaDB.product.findUnique({
+        where: {
+            id: id,
+        },
+        select: {
+            photo: true,
+            title: true,
+        },
+    });
+
+    if (!product) {
+        return notFound();
+    }
+
+    return (
+        <div className="absolute top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-60">
+            <CloseButton />
+            <div className="flex justify-center w-full max-w-screen-sm h-1/2">
+                <div className="relative flex items-center justify-center overflow-hidden rounded-md aspect-square bg-neutral-700 text-neutral-200">
+                    <Image
+                        src={`${product.photo}/public`}
+                        alt={product.title}
+                        fill
+                        className="object-cover"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
