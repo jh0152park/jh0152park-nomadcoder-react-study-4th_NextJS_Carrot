74a4beaf3877da2cefff8b07bb923bcdf738b3f3
jh0152park <jh0152park@naver.com>
2024-06-05 16:52:53 +0900

    fixed eslint issues

--- pepe-market/app/(tabs)/products/loading.tsx
+++ pepe-market/app/(tabs)/products/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
     return (
         <div className="flex flex-col gap-5 p-5 animate-pulse">
             {[...Array(10)].map((_, index) => (
-                <div className="*:rounded-md flex gap-5 ">
+                <div key={index} className="*:rounded-md flex gap-5 ">
                     <div className=" bg-neutral-700 size-28" />
                     <div className="flex flex-col gap-2 *:rounded-md">
                         <div className="w-40 h-5 bg-neutral-700" />
